b2ac5cb62bdcc6e24b48a75b9f86edadc8adfaf9
riakharisma <join.github@gmail.com>
2017-09-12 21:59:33 +0700

    Persiapan tahap pertama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DemoFormNgModelComponent } from './demo-form-ng-model/demo-form-ng-model.component';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
+import { DemoFormWithCustomValidationComponent } from './demo-form-with-custom-validation/demo-form-with-custom-validation.component';
+import { DemoFormWithEventsComponent } from './demo-form-with-events/demo-form-with-events.component';
+import { DemoFormWithValidationsExplicitComponent } from './demo-form-with-validations-explicit/demo-form-with-validations-explicit.component';
+import { DemoFormWithValidationsShorthandComponent } from './demo-form-with-validations-shorthand/demo-form-with-validations-shorthand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoFormNgModelComponent,
+    DemoFormSkuComponent,
+    DemoFormSkuWithBuilderComponent,
+    DemoFormWithCustomValidationComponent,
+    DemoFormWithEventsComponent,
+    DemoFormWithValidationsExplicitComponent,
+    DemoFormWithValidationsShorthandComponent
   ],
   imports: [
     BrowserModule
