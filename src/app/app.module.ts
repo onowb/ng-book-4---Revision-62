5b7b3d38841476c3a59a2aeaf51f3d0da68febb1
riakharisma <join.github@gmail.com>
2017-09-12 18:08:37 +0700

    Persiapan tahap pertama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgSwitchExampleComponent } from './ng-switch-example/ng-switch-example.component';
+import { NgClassExampleComponent } from './ng-class-example/ng-class-example.component';
+import { NgForExampleComponent } from './ng-for-example/ng-for-example.component';
+import { NgStyleExampleComponent } from './ng-style-example/ng-style-example.component';
+import { NgNonBindableExampleComponent } from './ng-non-bindable-example/ng-non-bindable-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgSwitchExampleComponent,
+    NgClassExampleComponent,
+    NgForExampleComponent,
+    NgStyleExampleComponent,
+    NgNonBindableExampleComponent
   ],
   imports: [
     BrowserModule
