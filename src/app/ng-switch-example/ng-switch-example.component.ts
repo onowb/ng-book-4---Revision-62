33fd8afb47c6baf048e55c03b9fe95d5068b35c4
riakharisma <join.github@gmail.com>
2017-09-13 12:05:55 +0700

    Cara menggunakan NgSwitch

--- src/app/ng-switch-example/ng-switch-example.component.ts
+++ src/app/ng-switch-example/ng-switch-example.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-switch-example.component.css']
 })
 export class NgSwitchExampleComponent implements OnInit {
+  choice: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.choice = 1;
+  }
+
+  nextChoice(): void {
+    this.choice += 1;
+
+    if (this.choice > 5) {
+      this.choice = 1;
+    }
   }
 
 }
