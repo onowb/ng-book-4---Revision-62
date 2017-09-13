f988701467ab0f552c26cae25cd1d36d21c09167
riakharisma <join.github@gmail.com>
2017-09-13 12:34:57 +0700

    Cara menggunakan NgStyle

--- src/app/ng-style-example/ng-style-example.component.ts
+++ src/app/ng-style-example/ng-style-example.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-style-example.component.css']
 })
 export class NgStyleExampleComponent implements OnInit {
+  color: string;
+  fontSize: number;
+  style: {
+    'background-color': string,
+    'border-radius': string,
+    border?: string,
+    width?: string,
+    height?: string
+  };
 
   constructor() { }
 
   ngOnInit() {
+    this.fontSize = 16;
+    this.color = 'blue';
+    this.style = {
+      'background-color': '#ccc',
+      'border-radius': '50px',
+      'height': '30px',
+      'width': '30px'
+    };
   }
 
+  apply(color: string, fontSize: number): void {
+    this.color = color;
+    this.fontSize = fontSize;
+  }
 }
