58fe3dab27ca304dbdfa21e1f3bc109387260035
riakharisma <join.github@gmail.com>
2017-09-14 19:14:38 +0700

    Cara menggunakan NgFor

--- src/app/ng-for-example/ng-for-example.component.ts
+++ src/app/ng-for-example/ng-for-example.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-for-example.component.css']
 })
 export class NgForExampleComponent implements OnInit {
+  cities: string[];
+  people: Object[];
+  peopleByCity: Object;
 
   constructor() { }
 
   ngOnInit() {
+    this.cities = ['Miami', 'Sao Paulo', 'New York'];
+    this.people = [
+      { name: 'Anderson', age: 35, city: 'Sao Paulo' },
+      { name: 'John', age: 12, city: 'Miami' },
+      { name: 'Peter', age: 22, city: 'New York' }
+    ];
+    this.peopleByCity = [
+      {
+        city: 'Miami',
+        people: [
+          { name: 'John', age: 12 },
+          { name: 'Angel', age: 22 }
+        ]
+      },
+      {
+        city: 'Sao Paulo',
+        people: [
+          { name: 'Anderson', age: 35 },
+          { name: 'Felipe', age: 36 }
+        ]
+      }
+    ];
   }
 
 }
