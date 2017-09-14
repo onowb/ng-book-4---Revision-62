340dc56aa7bb49a0ff1509e017cd693c00518ab5
riakharisma <join.github@gmail.com>
2017-09-14 21:41:28 +0700

    Persiapan tahap pertama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnalyticsDemoComponent } from './analytics-demo/analytics-demo.component';
+import { IntroComponent } from './intro/intro.component';
+import { PriceServiceDemoComponent } from './price-service-demo/price-service-demo.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UserDemoComponent } from './user-demo/user-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnalyticsDemoComponent,
+    IntroComponent,
+    PriceServiceDemoComponent,
+    SidebarComponent,
+    UserDemoComponent
   ],
   imports: [
     BrowserModule
