73146c2b24fe4ad6454c2d2cf0252df13899a7e2
riakharisma <join.github@gmail.com>
2017-09-15 07:09:20 +0700

    Finis persiapan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoreHttpRequestsComponent } from './more-http-requests/more-http-requests.component';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoreHttpRequestsComponent,
+    SimpleHttpComponent,
+    YouTubeSearchComponent
   ],
   imports: [
     BrowserModule
