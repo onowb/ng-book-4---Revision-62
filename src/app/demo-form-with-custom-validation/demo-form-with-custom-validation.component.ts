3fefe273626646882719a085a124345b26a2ab2b
riakharisma <join.github@gmail.com>
2017-09-15 22:13:18 +0700

    demo-form-with-custom-validation Sukses

--- src/app/demo-form-with-custom-validation/demo-form-with-custom-validation.component.ts
+++ src/app/demo-form-with-custom-validation/demo-form-with-custom-validation.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+/**
+ * Our custom validator
+ *
+ * A validator:
+ * - Takes a `Control` as it's input and
+ * - Returns a `StringMap<string, boolean>` where the key is "error code" and
+ *   the value is `true` if it fails
+ */
+
+function skuValidator(control: FormControl): {
+  [s: string]: boolean
+} {
+  if (!control.value.match(/^123/)) {
+    return {
+      invalidSku: true
+    };
+  }
+}
 
 @Component({
   selector: 'app-demo-form-with-custom-validation',
@@ -6,8 +26,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-form-with-custom-validation.component.css']
 })
 export class DemoFormWithCustomValidationComponent implements OnInit {
+  myFrom: FormGroup;
+  sku: AbstractControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.myFrom = fb.group({
+      'sku': ['', Validators.compose([
+        Validators.required, skuValidator
+      ])]
+    });
+
+    this.sku = this.myFrom.controls['sku'];
+  }
+
+  onSubmit(value: string): void {
+    console.log('You submitted value: ', value);
+  }
 
   ngOnInit() {
   }
