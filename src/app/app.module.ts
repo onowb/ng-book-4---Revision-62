88cafdcbe6caf0cb4bb17993e0b81cac8f8593f1
riakharisma <join.github@gmail.com>
2017-09-15 08:52:31 +0700

    Persiapan tahap pertama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
+import { ArtistComponent } from './artist/artist.component';
+import { SearchComponent } from './search/search.component';
+import { TrackComponent } from './track/track.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent,
+    ArtistComponent,
+    SearchComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule
