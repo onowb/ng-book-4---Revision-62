2c679c5447e39e827f0e2f4490ab987e28da15ef
riakharisma <join.github@gmail.com>
2017-09-16 17:30:20 +0700

    Injections Example: PriceService

---
+++ src/app/price-service-demo/product.spec.ts
@@ -0,0 +1,18 @@
+import { Product } from './product.model';
+import { MockPriceService } from './price.service.mock';
+import {beforeEach} from 'selenium-webdriver/testing';
+
+describe('Product', () => {
+  let product;
+
+  beforeEach(() => {
+    const service = new MockPriceService();
+    product = new Product(service, 11.00);
+  });
+
+  describe('price', () => {
+    it('is calculated based on the basePrice and the state', () => {
+      expect(product.totalPrice('FL')).toBe(11.66);
+    });
+  });
+});
