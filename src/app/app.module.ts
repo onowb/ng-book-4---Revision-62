9d70981f3edf69d972d252c815a42d37ab363583
riakharisma <join.github@gmail.com>
2017-09-18 14:26:42 +0700

    Persiapan tahap Pertama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { MessagesService } from './messages/messages.service';
+import { UserService } from './user/user.service';
+import { ThreadService } from './thread/thread.service';
 
 import { AppComponent } from './app.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatMessageComponent,
+    ChatNavBarComponent,
+    ChatPageComponent,
+    ChatThreadComponent,
+    ChatThreadsComponent,
+    ChatWindowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [MessagesService, UserService, ThreadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
