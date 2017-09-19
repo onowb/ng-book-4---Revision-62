87f98067b432c4393e809f22006b285369a11fef
riakharisma <join.github@gmail.com>
2017-09-19 21:50:33 +0700

    Langkah Pertama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
+import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { FromNowPipe } from './pipes/from-now.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatMessageComponent,
+    ChatThreadComponent,
+    ChatNavBarComponent,
+    ChatThreadsComponent,
+    ChatWindowComponent,
+    ChatPageComponent,
+    FromNowPipe
   ],
   imports: [
     BrowserModule
