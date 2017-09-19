3ee06f1d32873caf32286427a428b50530f1cfed
riakharisma <join.github@gmail.com>
2017-09-19 16:28:29 +0700

    Masih sama 3

--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -17,15 +17,15 @@ interface IMessagesOperation extends Function {
 
 @Injectable()
 export class MessagesService {
-  // a stream that publishes new message only once
+  // a stream that publishes new messages only once
   newMessages: Subject<Message> = new Subject<Message>();
 
-  // `message` is a stream that emits an array of the most up to date message
+  // `messages` is a stream that emits an array of the most up to date messages
   messages: Observable<Message[]>;
 
-  // `updates` receives _operations_ to be applied to our `message`
-  // it's a way we can perform changes on *all* message (that are currently
-  // stored in `message`)
+  // `updates` receives _operations_ to be applied to our `messages`
+  // it's a way we can perform changes on *all* messages (that are currently
+  // stored in `messages`)
   updates: Subject<any> = new Subject<any>();
 
   // action streams
@@ -34,17 +34,20 @@ export class MessagesService {
 
   constructor() {
     this.messages = this.updates
-    // watch the updates and accumulate operations on the message
-      .scan((messages: Message[], operation: IMessagesOperation) => {
-      return operation(messages);
-    }, initialMessages)
-    // make sure we can share the most recent list of message across anyone
-    // who's interested in subscribing and cache the last known list of
-    // message
-    .publishReplay(1).refCount();
+    // watch the updates and accumulate operations on the messages
+      .scan((messages: Message[],
+             operation: IMessagesOperation) => {
+          return operation(messages);
+        },
+        initialMessages)
+      // make sure we can share the most recent list of messages across anyone
+      // who's interested in subscribing and cache the last known list of
+      // messages
+      .publishReplay(1)
+      .refCount();
 
     // `create` takes a Message and then puts an operation (the inner function)
-    // on the `updates` stream to add the Message to the list of message.
+    // on the `updates` stream to add the Message to the list of messages.
     //
     // That is, for each item that gets added to `create` (by using `next`)
     // this stream emits a concat operation function.
@@ -57,29 +60,35 @@ export class MessagesService {
     // the update stream directly and get rid of this extra action stream
     // entirely. The pros are that it is potentially clearer. The cons are that
     // the stream is no longer composable.
-    this.create.map(function (message: Message): IMessagesOperation {
-      return (messages: Message[]) => {
-        return messages.concat(message);
-      };
-    }).subscribe(this.updates);
+    this.create
+      .map( function(message: Message): IMessagesOperation {
+        return (messages: Message[]) => {
+          return messages.concat(message);
+        };
+      })
+      .subscribe(this.updates);
 
-    this.newMessages.subscribe(this.create);
+    this.newMessages
+      .subscribe(this.create);
 
     // similarly, `markThreadAsRead` takes a Thread and then puts an operation
     // on the `updates` stream to mark the Messages as read
-    this.markThreadAsRead.map((thread: Thread) => {
-      return (messages: Message[]) => {
-        return messages.map((message: Message) => {
-          // note that we're manipulating `message` directly here. Mutability
-          // can be confusing and there are lots of reasons why you might want
-          // to, say, copy the Message object or some other 'immutable' here
-          if (message.thread.id === thread.id) {
-            message.isRead = true;
-          }
-          return message;
-        });
-      };
-    }).subscribe(this.updates);
+    this.markThreadAsRead
+      .map( (thread: Thread) => {
+        return (messages: Message[]) => {
+          return messages.map( (message: Message) => {
+            // note that we're manipulating `message` directly here. Mutability
+            // can be confusing and there are lots of reasons why you might want
+            // to, say, copy the Message object or some other 'immutable' here
+            if (message.thread.id === thread.id) {
+              message.isRead = true;
+            }
+            return message;
+          });
+        };
+      })
+      .subscribe(this.updates);
+
   }
 
   // an imperative function call to this action stream
@@ -88,7 +97,8 @@ export class MessagesService {
   }
 
   messagesForThreadUser(thread: Thread, user: User): Observable<Message> {
-    return this.newMessages.filter((message: Message) => {
+    return this.newMessages
+      .filter((message: Message) => {
         // belongs to this thread
         return (message.thread.id === thread.id) &&
           // and isn't authored by this user
